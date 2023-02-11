76b1f57c581f91dbd0ef262ae2c0253ee9e6b57a
Bryan Gámez <bg6907992@gmail.com>
2023-02-10 22:30:10 -0600

    Agregando funcionalidades con JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -164,3 +164,18 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+//Enviar Correo
+
+const $form = document.getElementById('form');
+const $buttonMailto = document.getElementById('trucazo');
+
+$form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = new FormData(this);
+  console.log(form.get('nombre'));
+  $buttonMailto.setAttribute('href', `mailto:bg6907992@gmail.com?subject=nombre: ${form.get('nombre')} correo: ${form.get('email')} telefono: ${form.get('phone')}&body=${form.get('mensaje')}`)
+  $buttonMailto.click();
+}
