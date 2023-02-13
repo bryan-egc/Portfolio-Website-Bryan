380686e76b6fa03da1f3d39cd27b319d529246bf
Bryan Gámez <bg6907992@gmail.com>
2023-02-12 19:48:52 -0600

    Guardando cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -168,14 +168,9 @@ themeButton.addEventListener('click', () => {
 //Enviar Correo
 
 const $form = document.getElementById('form');
-const $buttonMailto = document.getElementById('trucazo');
 
 $form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
-  const form = new FormData(this);
-  console.log(form.get('nombre'));
-  $buttonMailto.setAttribute('href', `mailto:bg6907992@gmail.com?subject=nombre: ${form.get('nombre')} correo: ${form.get('email')} telefono: ${form.get('phone')}&body=${form.get('mensaje')}`)
-  $buttonMailto.click();
 }
